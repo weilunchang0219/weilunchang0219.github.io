3ed60dde29ced40896854955e19703cc1bae9af4
weilunchang0219 <83078572+weilunchang0219@users.noreply.github.com>
2025-04-28 03:11:40 +0000

    Deploying to gh-pages from @ weilunchang0219/weilunchang0219.github.io@e13dae7d1ab69f32dabc1e8ae6405a40e86cb087 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -74,6 +74,11 @@ ninja.data = [{
           description: "為Eurobot比賽設計的整合式底盤控制系統",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_competition_chassis_pcb/";
+            },},{id: "projects-esp32-s3-小型機器人控制板",
+          title: 'ESP32-S3 小型機器人控制板',
+          description: "基於ESP32-S3-WROOM-1U-N16R8所設計的高度整合控制板，集成步進馬達驅動、多種感測器接口和電源管理，專為Eurobot競賽設計。",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_competition_esp32_robot_controller_board/";
             },},{
         id: 'social-email',
         title: 'email',
